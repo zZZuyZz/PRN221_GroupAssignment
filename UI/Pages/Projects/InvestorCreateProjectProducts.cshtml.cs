1219a2f1f7454a4b4a3ebe9a924a11f1c31cf9a1
Duy Lieng <duylieng.ntt@gmail.com>
2024-03-17 10:51:02 +0700

    UI for Ivestor create project and products

---
+++ UI/Pages/Projects/InvestorCreateProjectProducts.cshtml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using BO.Models;
+
+namespace UI.Pages.Projects
+{
+    public class InvestorCreateProjectProductsModel : PageModel
+    {
+        private readonly BO.Models.RealEstateManagementContext _context;
+
+        public InvestorCreateProjectProductsModel(BO.Models.RealEstateManagementContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Project Project { get; set; }
+
+        [BindProperty]
+        public List<Product> Products { get; set; }
+
+        public void OnGet()
+        {
+            Products = new List<Product> { new Product() }; // Add an initial product
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Projects.Add(Project);
+            await _context.SaveChangesAsync();
+
+            foreach (var product in Products)
+            {
+                product.ProjectId = Project.Id; // Assign projectId to products
+                _context.Products.Add(product);
+            }
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("/Index");
+        }
+
+        public void AddProduct()
+        {
+            Products.Add(new Product());
+        }
+    }
+}
